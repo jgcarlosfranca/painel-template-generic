473b1fc93683837efc8729f241a823557ffc1ed2
JoaoGCarlosF <joaogcarlosf@gmail.com>
2021-12-24 16:56:22 -0300

    tirando subtituloo

--- src/Components/FormBasics/FormLayout/FormHeader.jsx
+++ src/Components/FormBasics/FormLayout/FormHeader.jsx
@@ -8,7 +8,7 @@ export const FormHeader = ({
   return (
     <header className="header__title">
       <h2 className="header__title header__title--primary ">{titulo}</h2>
-      <span className="header__subtitle header__subtitle--secondary" hidden={!!subtitulo}>
+      <span className="header__subtitle header__subtitle--secondary" >
         {subtitulo}
       </span>
     </header>
