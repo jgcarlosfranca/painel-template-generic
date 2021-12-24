b8faa2e474dcac501ae83bad8cef573565070bc7
JoaoGCarlosF <joaogcarlosf@gmail.com>
2021-12-24 17:51:49 -0300

    useState on textInput

--- src/Pages/PokeDex/index.jsx
+++ src/Pages/PokeDex/index.jsx
@@ -1,15 +1,19 @@
+import React, { useState } from "react";
 import { FormHeader } from "../../Components/FormBasics/FormLayout/FormHeader";
 import { Divider } from "../../Components/FormBasics/Divider"
 import { TextInput } from "../../Components/FormBasics/TextInput"
 import "./styles.css";
 
 export default function Pokedex() {
+  const [nomeGeracao, setNomeGeracao] = useState('1');
+
   return (
     <div className="home">
       <div className="forms__general">
         <FormHeader titulo="Busca de Pokemons" />
         <Divider />
-        <TextInput titulo="Nome da geração" />
+        <TextInput titulo="indice do pokemon" placeholder="digite o numero do pokemon a ser buscado" tamanho="23%"
+          onChange={(ev) => { setNomeGeracao(ev.target.value.replace(/[^0-9]/g, '')) }} />
       </div>
     </div>
   );
