b796b9bbae97b06995373dc1f19b26f33ef25ef1
JoaoGCarlosF <joaogcarlosf@gmail.com>
2021-12-25 00:05:10 -0300

    button clean

--- src/Pages/PokeDex/index.jsx
+++ src/Pages/PokeDex/index.jsx
@@ -3,14 +3,21 @@ import { FormHeader } from "../../Components/FormBasics/FormLayout/FormHeader";
 import { Divider } from "../../Components/FormBasics/Divider"
 import { TextInput } from "../../Components/FormBasics/TextInput"
 import { Button } from "../../Components/FormBasics/Button"
+import { FaEraser } from "react-icons/fa"
 import axios from "axios";
 
 import "./styles.css";
 
 export default function Pokedex() {
-  const [idPokemon, setIdPokemon] = useState('1');
+  const [idPokemon, setIdPokemon] = useState('');
   const [canShowPokemon, setCanShowPokemon] = useState(false)
   const [pokemon, setPokemon] = useState('')
+
+  const limpar = () => {
+    setCanShowPokemon(false)
+    setIdPokemon('')
+    setPokemon('')
+  }
 
   const buscaDePokemon = () => {
     let config = {
@@ -34,12 +41,30 @@ export default function Pokedex() {
       <div className="forms__general">
         <FormHeader titulo="Busca de Pokemons" />
         <Divider />
-        <TextInput titulo="indice do pokemon" placeholder="digite o numero do pokemon a ser buscado" tamanho="23%"
-          onChange={(ev) => { setIdPokemon(ev.target.value.replace(/[^0-9]/g, '')) }} />
+        <div className="form_body">
+          <TextInput titulo="indice do pokemon" placeholder="digite o numero do pokemon a ser buscado" tamanho="23%"
+            value={idPokemon}
+            onChange={(ev) => { setIdPokemon(ev.target.value.replace(/[^0-9]/g, '')) }} />
+
+          <FaEraser onClick={limpar} />
+        </div>
         <Button cor={'#16bf6e'} titulo="Buscar" onClick={buscaDePokemon} />
+
+
         <div className="search" hidden={!canShowPokemon}>
           {pokemon.name}
-          <img src={pokemon ? pokemon['sprites']['other']['official-artwork']['front_default'] : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'}></img>
+          <div className="search_img" hidden={!canShowPokemon}>
+            <img src={pokemon ? pokemon['sprites']['other']['official-artwork']['front_default'] : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'}></img>
+            <img
+              src={pokemon
+                ? pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+                : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'}></img>
+            <img
+              src={pokemon
+                ? pokemon['sprites']['versions']['generation-v']['black-white']['animated']['back_default']
+                : 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'}></img>
+
+          </div>
         </div>
       </div>
     </div>
