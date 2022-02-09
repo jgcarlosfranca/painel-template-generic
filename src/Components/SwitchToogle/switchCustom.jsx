0dc4f2a239131af85568922e4404d0ddc58218d9
joaogcarlosf <joaogcarlosf@gmail.com>
2022-02-08 23:28:31 -0300

    criacao do switch custom

---
+++ src/Components/SwitchToogle/switchCustom.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+const ToggleSwitch = (props) => {
+    return (
+        <div className="container">
+            <div className="toggle-switch">
+                <input type="checkbox" className="checkboxCustom"
+                    name={props.name} id={props.label} checked={props.valor} onChange={props.handleChangeCheckBox} />
+                <label className="checkboxCustomInner" htmlFor={props.label}>
+                    <span className="innerBackground" />
+                    <span className="switch" />
+                </label>
+            </div>
+        </div>
+    );
+};
+
+export default ToggleSwitch;
