029bfb97aff5096267d742401962f503bb11dcc4
joaogcarlosf <joaogcarlosf@gmail.com>
2022-02-28 19:27:17 -0300

    creating switch toogle on forms page

--- src/Pages/Formulario/index.jsx
+++ src/Pages/Formulario/index.jsx
@@ -1,10 +1,15 @@
 import "./styles.css";
+import ToggleSwitch from "../../Components/SwitchToogle/switchCustom";
+import React, {useState} from "react";
 
 
 export default function Home() {
+  const [ligado, setLigado] = useState('')
+
   return (
     <div className="home">
       Hello world
+      <ToggleSwitch name="teste_Switch_toogle" valor={ligado} label="teste" handleChangeCheckBox={() => setLigado(!ligado)}/>
     </div>
   );
 }
