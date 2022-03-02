89afb87bc995915fbf031270414ef38e83195c31
joaogcarlosf <joaogcarlosf@gmail.com>
2022-03-02 19:47:30 -0300

    creating more elements to forms

--- src/Pages/Formulario/index.jsx
+++ src/Pages/Formulario/index.jsx
@@ -1,18 +1,28 @@
-import "./styles.css";
-import ToggleSwitch from "../../Components/SwitchToogle/switchCustom";
 import React, { useState } from "react";
-
+import ToggleSwitch from "../../Components/SwitchToogle/switchCustom";
+import { FormHeader } from "../../Components/FormBasics/FormLayout/FormHeader";
+import { Divider } from "../../Components/FormBasics/Divider"
+import { TextInput } from "../../Components/FormBasics/TextInput"
+import { Button } from "../../Components/FormBasics/Button"
+import "./styles.css";
 
 export default function Forms() {
   const [ligado, setLigado] = useState('')
+  const [textInput1, setTextInput1] = useState('');
 
   return (
     <div className="home">
-      <div className="row_form">
-        Hello world
-      </div>
-      <div className="row_form">
-        <ToggleSwitch name="teste_Switch_toogle" valor={ligado} label="teste" handleChangeCheckBox={() => setLigado(!ligado)} />
+      <div className="forms__general">
+        <FormHeader titulo="Busca de Pokemons" />
+        <Divider />
+        <div className="form_body">
+          <TextInput titulo="Input text" placeholder="digite um texto" tamanho="23%"
+            value={textInput1}
+            onChange={(ev) => { setTextInput1(ev.target.value) }} />
+          <ToggleSwitch name="teste_Switch_toogle" valor={ligado} label="teste" handleChangeCheckBox={() => setLigado(!ligado)} />
+
+        </div>
+        <Button cor={'#16bf6e'} corHover={'#06311d'} titulo="Buscar" onClick={() => { console.log('clicou') }} />
       </div>
     </div>
   );
