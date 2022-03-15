ef61a71834af59164dafb6c66d00c5c1afc38dd3
joaogcarlosf <joaogcarlosf@gmail.com>
2022-03-15 08:56:32 -0300

    creating div to glass effect

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,7 +4,13 @@ import "./styles.css";
 export default function Forms() {
   return (
     <div className="home">
+      <div className="conteiner_glass_effect">
+
+      </div>
       Hello world
+      <div className="conteiner_glass_effect">
+
+      </div>
     </div>
   );
 }
