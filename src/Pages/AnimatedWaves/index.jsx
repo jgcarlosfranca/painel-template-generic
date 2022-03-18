e0de34cc549469e2ebf4b1cfc7d264d989ebf681
joaogcarlosf <joaogcarlosf@gmail.com>
2022-03-17 21:43:39 -0300

    test glass effect

--- src/Pages/AnimatedWaves/index.jsx
+++ src/Pages/AnimatedWaves/index.jsx
@@ -4,6 +4,8 @@ import "./styles.css";
 export default function AnimatedWaves() {
   return (
     <div className="home">
+      <div className="MainPainel">
+        </div>
 
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,16 +4,19 @@ import "./styles.css";
 export default function Forms() {
   return (
     <div className="home">
-      <div className="conteiner_glass_effect">
-        
-      </div>
-      <div className="conteiner_ghost_white">
-        <div className="animated_digitation">
-          JG dev Portifolio Welcome   !!!!
-        </div>
-      </div>
-      <div className="conteiner_glass_effect">
+      <div className="MainPainel">
 
+        <div className="conteiner_glass_effect">
+
+        </div>
+        <div className="conteiner_ghost_white">
+          <div className="animated_digitation">
+            JG dev Portifolio Welcome   !!!!
+          </div>
+        </div>
+        <div className="conteiner_glass_effect">
+
+        </div>
       </div>
     </div>
   );
