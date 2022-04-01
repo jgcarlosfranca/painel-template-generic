7955a51ad9bacea5e26c89ed47dcf57b3db712c5
joaogcarlosf <joaogcarlosf@gmail.com>
2022-03-31 22:08:14 -0300

    flex end

--- src/Components/PokeComponents/PokeSkillCard/PokeSkillCard.styles.jsx
+++ src/Components/PokeComponents/PokeSkillCard/PokeSkillCard.styles.jsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledPokeSkillCard = styled.div`
 display: flex;
 flex-direction: column;
+align-items: flex-end;
 margin: 1em;
-width: 45%;
+width: 25%;
 `;
