743fd985aa3f0db27f62718dd813cc1be6b21894
joaogcarlosf <joaogcarlosf@gmail.com>
2022-04-01 22:42:16 -0300

    fixing center item

--- src/Components/PokeComponents/PokeSkillCard/PokeSkillCard.styles.jsx
+++ src/Components/PokeComponents/PokeSkillCard/PokeSkillCard.styles.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const StyledPokeSkillCard = styled.div`
 display: flex;
 flex-direction: column;
-align-items: flex-end;
+align-items: center;
 margin: 1em;
 width: 25%;
 `;
