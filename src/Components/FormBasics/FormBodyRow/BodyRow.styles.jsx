e492987c8d5b6d79baf29041792bfc3ceb4c8625
joaogcarlosf <joaogcarlosf@gmail.com>
2022-04-15 22:27:44 -0300

    testing body row

--- src/Components/FormBasics/FormBodyRow/BodyRow.styles.jsx
+++ src/Components/FormBasics/FormBodyRow/BodyRow.styles.jsx
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const StyledFormBodyRow = styled.div`
 display: flex;  
+flex-direction: row;
 `;
--- src/Pages/Formulario/index.jsx
+++ src/Pages/Formulario/index.jsx
@@ -5,6 +5,7 @@ import { Divider } from "../../Components/FormBasics/Divider"
 import { TextInput } from "../../Components/FormBasics/TextInput"
 import { Button } from "../../Components/FormBasics/Button"
 import { CustomSelect } from "../../Components/FormBasics/Select";
+import { FormBodyRow } from "../../Components/FormBasics/FormBodyRow/BodyRow";
 import "./styles.css";
 
 export default function Forms() {
@@ -17,11 +18,13 @@ export default function Forms() {
         <FormHeader titulo="Formulario Exemplo" cor='#16bf6e' />
         <Divider />
         <div className="form_body">
-          <TextInput titulo="Input text" placeholder="digite um texto" tamanho="30%"
-            value={textInput1}
-            onChange={(ev) => { setTextInput1(ev.target.value) }} />
-          <CustomSelect />
-          <ToggleSwitch name="teste_Switch_toogle" valor={ligado} label="teste" handleChangeCheckBox={() => setLigado(!ligado)} />
+          <FormBodyRow>
+            <TextInput titulo="Input text" placeholder="digite um texto" tamanho="30%"
+              value={textInput1}
+              onChange={(ev) => { setTextInput1(ev.target.value) }} />
+            <CustomSelect />
+            <ToggleSwitch name="teste_Switch_toogle" valor={ligado} label="teste" handleChangeCheckBox={() => setLigado(!ligado)} />
+          </FormBodyRow>
 
         </div>
         <Button cor={'#16bf6e'} corHover={'#06311d'} titulo="Buscar" onClick={() => { console.log('clicou') }} />
