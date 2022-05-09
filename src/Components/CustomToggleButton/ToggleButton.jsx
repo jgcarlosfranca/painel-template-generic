5d58b9b918e0c9754cfaa96d8cb9d86dcfc5cb4a
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-09 10:28:17 -0300

    imports icons to custom toggle

--- src/Components/CustomToggleButton/ToggleButton.jsx
+++ src/Components/CustomToggleButton/ToggleButton.jsx
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { StyledToggle } from "./ToggleButton.styles";
+import {BsSun, BsMoon} from "react-icons/bs"
 
 export function Button(props) {
-  const { checked, iconTrue, iconFalse, cor} = props;
+  const { checked, iconTrue = BsSun, iconFalse = BsMoon, cor} = props;
 
   return (
     <StyledToggle cor={cor} iconFalse={iconFalse} iconTrue={iconTrue} checked={checked}>
