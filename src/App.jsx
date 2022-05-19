ced0f86dc5af09964ba4c6e461c55e3896def24d
joaogcarlosf <joaogcarlosf@gmail.com>
2022-05-18 21:55:43 -0300

    creating route to study grid and flex

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Topbar />
+      <Topbar />
         <div className="container">
           <Sidebar />
           <Routes>
---
+++ src/Pages/GridFlex/index.jsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+
+export default function GridFlex() {
+  return (
+    <div className="home">
+      
+    </div>
+  );
+}
